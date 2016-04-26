efd6ff1fcac596327a2dd34eb2dd6e3b4907a410
Jacob Taylor <jacob@usekahuna.com>
2016-04-25 17:14:07 -0700

    finishing refactor to send_message function. export list is working. Time for cleanup

--- server.go
+++ server.go
@@ -10,6 +10,38 @@ import (
     "encoding/binary"
 )
 
+func send_message(output *bufio.Writer, reply_type uint32, length uint32, data []byte ) {
+    endian := binary.BigEndian
+    buffer := make([]byte, 1024)
+    offset := 0
+
+    endian.PutUint64(buffer[offset:], utils.NBD_SERVER_SEND_REPLY_MAGIC)
+    offset += 8
+
+    endian.PutUint32(buffer[offset:], uint32(3))  // Flags (3 = supports list)
+    offset += 4
+
+    endian.PutUint32(buffer[offset:], reply_type)  // reply_type: NBD_REP_SERVER
+    offset += 4
+
+    // length of export name package
+    endian.PutUint32(buffer[offset:], length)  // length of string
+    offset += 4
+
+    fmt.Printf("offset is: %4d  length is: %4d\n", offset, length)
+    //if length > 0 {
+        copy(buffer[offset:], data[0:length])
+        offset += int(length)
+    //}
+
+    data_to_send := buffer[:offset]
+    output.Write(data_to_send)
+    output.Flush()
+
+    utils.LogData("Just sent:", offset, data_to_send)
+}
+
+
 func main() {
     controlC := [...]byte{255, 244, 255, 253, 6}
     listener, err := net.Listen("tcp", "192.168.214.1:8000")
@@ -45,39 +77,17 @@ func main() {
         utils.ErrorCheck(err)
         utils.LogData("B", length, data)
 
-        endian := binary.BigEndian
-
-
         // Send the first export
-
-
-        //output.Write(reply_magic)
+        endian := binary.BigEndian
         data = make([]byte, 1024)
-        offset := 0
-        endian.PutUint64(data[offset:], utils.NBD_SERVER_SEND_REPLY_MAGIC)
-        offset += 8
-        //output.WriteString(">EVZ")
-        //output.Write(data[0:7])
-        //output.Flush()
-        //output.WriteString("IHAVEOPT")      // Magic
-        //output.Flush()
-        endian.PutUint32(data[offset:], uint32(3))  // Flags (3 = supports list)
-        offset += 4
-        //output.Write([]byte{0, 3})
-        //output.Flush()
-        // reply type
-        endian.PutUint32(data[offset:], uint32(2))  // reply_type: NBD_REP_SERVER
-        offset += 4
-
-        //output.Write([]byte{0, 2})          // reply_type: NBD_REP_SERVER
-        //output.Flush()
-
         export_name := "happy_export"
         length = len(export_name)
 
-        // length of export name package
-        endian.PutUint32(data[offset:], uint32(length + 4))  // length of string
-        offset += 4
+        offset := 0
+
+        //// length of export name package
+        //endian.PutUint32(data[offset:], uint32(length + 4))  // length of all data (string + size)
+        //offset += 4
 
         // length of export name
         endian.PutUint32(data[offset:], uint32(length))  // length of string
@@ -87,24 +97,42 @@ func main() {
         copy(data[offset:], export_name)
         offset += length
 
-        output.Write(data)
-        output.Flush()
-
-        utils.LogData("Just sent:", offset, data)
-
-        //output.Write(utils.EncodeInt(len(export_name)))
-        //// export name
-        //output.WriteString(export_name)
-        //output.Write([]byte{})
-        //output.Flush()
-
-        //data = make([]byte, 1024)
-        //length, err = conn.Read(data)
-        //utils.ErrorCheck(err)
-        //utils.LogData("C", length, data)
+        reply_type := uint32(2)     // reply_type: NBD_REP_SERVER
+        utils.LogData("Calling send_message with:", offset, data)
+        send_message(output, reply_type, uint32(offset), data)
 
 
 
+        // Send the second export
+        data = make([]byte, 1024)
+        export_name = "Very_happy_export"
+        length = len(export_name)
+        offset = 0
+
+        // length of export name
+        endian.PutUint32(data[offset:], uint32(length))  // length of string
+        offset += 4
+
+        // export name
+        copy(data[offset:], export_name)
+        offset += length
+
+        reply_type = uint32(2)     // reply_type: NBD_REP_SERVER
+        utils.LogData("Calling send_message with:", offset, data)
+        send_message(output, reply_type, uint32(offset), data)
+
+
+
+
+
+
+
+        // Send acknowledgement that the list is done.
+
+        reply_type = uint32(1)      // reply_type: NBD_REP_ACK
+
+        data = make([]byte, 1024)
+        send_message(output, reply_type, 0, data)
 
         input := bufio.NewScanner(conn)
         for input.Scan() {
