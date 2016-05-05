b1ea0537cc0436125724c934abe23b19573a238c
Jacob Taylor <jacob@usekahuna.com>
2016-05-04 18:21:02 -0700

    cleaning up unused code

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"os"
 	"bufio"
     "encoding/binary"
-    "bytes"
     "reflect"
     "./utils"
 )
@@ -63,49 +62,13 @@ func main() {
 
     fmt.Printf("%s\n", reflect.TypeOf(utils.NBD_REQUEST_MAGIC))
 
-    //data = make([]byte, 1000)
-    //request.reqtype = 3
-    //request.encodeRequest(data)
-
     newline := make([]byte, 1)
     newline[0] = byte('\n')
     writer.Write(newline)
-    //writer.Write(data[0:32])
-    //writer.Flush()
 
     count, err = reader.Read(data)
     utils.ErrorCheck(err)
     utils.LogData("B", count, data)
-
-    //count, err = reader.Read(data)
-    //utils.ErrorCheck(err)
-    //utils.LogData("C", count, data)
-
-    //tempData := make([]byte, 4)
-    ////outputBuffer := bytes.NewBufferString("NBDMAGIC")
-    //outputBuffer := new(bytes.Buffer)
-    //binary.BigEndian.PutUint32(tempData, NBD_REPLY_MAGIC)  // request list
-    //outputBuffer.Write(tempData)
-    //logBuffer("senda", outputBuffer)
-    //
-    //binary.BigEndian.PutUint32(tempData, 3)  // request list
-    //outputBuffer.Write(tempData)
-    //logBuffer("sendb", outputBuffer)
-    //
-    //binary.BigEndian.PutUint32(tempData, 0)  // length
-    //outputBuffer.Write(tempData)
-    //logBuffer("sendc", outputBuffer)
-    //
-    //outputBuffer.WriteByte('\n')
-    //logBuffer("sendd", outputBuffer)
-    //
-    //count, err = writer.Write(outputBuffer.Bytes())
-    //writer.Flush()
-    //utils.ErrorCheck(err)
-
-    //count, err = reader.Read(data)
-    //utils.ErrorCheck(err)
-    //utils.LogData("B", count, data)
 
     // send out options and the request for a list
     count, err = writer.Write([]byte{0, 0, 0, 3, 73, 72, 65, 86, 69, 79, 80, 84})
@@ -129,10 +92,6 @@ func main() {
 	http.HandleFunc("/", receive)
 	http.HandleFunc("/start", start)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-func logBuffer(msg string, buffer *bytes.Buffer) {
-    fmt.Printf("%5s (count %3d) Data: '%s' (%v)\n", msg, buffer.Len(), string(buffer.Bytes()), buffer.Bytes())
 }
 
 func receive(w http.ResponseWriter, r *http.Request) {
