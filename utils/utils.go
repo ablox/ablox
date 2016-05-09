35bbc02051764bc31b4c6ba5866083a40f171277
Jacob Taylor <jacob@usekahuna.com>
2016-05-09 12:10:28 -0700

    adding all request commands

--- utils/utils.go
+++ utils/utils.go
@@ -13,8 +13,13 @@ const (
     NBD_REQUEST_MAGIC =                 uint32(0x25609513)
     NBD_REPLY_MAGIC =                   uint32(0x67446698)
     NBD_SERVER_SEND_REPLY_MAGIC =       uint64(0x3e889045565a9)
-
     NBD_COMMAND_ACK =                   uint32(1)
+
+    NBD_COMMAND_READ =                  uint32(0)
+    NBD_COMMAND_WRITE =                 uint32(1)
+    NBD_COMMAND_DISCONNECT =            uint32(2)
+    NBD_COMMAND_FLUSH =                 uint32(3)
+    NBD_COMMAND_TRIM =                  uint32(4)
 
     NBD_COMMAND_EXPORT_NAME =           uint32(1)
     NBD_COMMAND_LIST =                  uint32(3)
